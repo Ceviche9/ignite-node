20ca42d8eab5ade1c131e353da8e25d3f9de0426
Tundê <ayotunde_sales@hotmail.com>
2022-02-05 23:50:49 -0300

    add chapter 2

--- 01 - FinAPI/src/index.js
+++ 01 - FinAPI/src/index.js
@@ -16,6 +16,35 @@ const customers = [];
 
 */
 
+// Middleware
+function verifyIfAccountCPFExists(request, response, next) {
+  const {cpf} = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if(!customer) {
+    return response.status(400).json({error: "Customer not found"});
+  }
+
+  // para que todas as rotas que usam esse middleware tenha acesso a essa informação. 
+  request.customer = customer;
+
+  return next()
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === "credit") {
+      return acc + operation.amount
+    }
+
+    return acc - operation.amount
+  }, 0)
+  
+
+  return balance;
+}
+
 app.post('/account', (request, response) => {
   const {cpf, name} = request.body;
 
@@ -37,12 +66,48 @@ app.post('/account', (request, response) => {
   return response.status(201).send()
 })
 
-app.get('/statement/:cpf', (request, response) => {
-  const {cpf} = request.params;
-
-  const customer = customers.find(customer => customer.cpf === cpf)
+app.get('/statement', verifyIfAccountCPFExists, (request, response) => {
+  const {customer} = request;
 
   return response.json(customer.statement);
+})
+
+app.post('/deposit', verifyIfAccountCPFExists, (request, response) => {
+  const {description, amount} = request.body;
+
+  const {customer} = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit'
+  }
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+})
+
+app.post('/withdraw', verifyIfAccountCPFExists, (request, response) => {
+  const { amount } = request.body;
+  const {customer} = request;
+
+  const balance = getBalance(customer.statement)
+
+  if(balance < amount) {
+    return response.status(400).json({error: "Amount out of range"})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit'
+  }
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
 })
 
 app.listen(3333, () => console.log("Server is running"))
