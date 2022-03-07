565721f2a5cd82659cd5399feb988f0f4388b1d4
Tundê <ayotunde_sales@hotmail.com>
2022-03-07 12:17:22 -0300

    feat(user): add user avatar upload

--- 02 - CarLoan/src/routes/users.routes.ts
+++ 02 - CarLoan/src/routes/users.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from "express"
+import multer from "multer";
+import { ensureAthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/UpdateUserAvatar/UpdateUserAvatarController";
 
+import uploadConfig from "../config/upload"
+
 const usersRoutes = Router();
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"))
 
 const createUserController = new CreateUserController()
 const updateUserAvatarController = new UpdateUserAvatarController()
 
 usersRoutes.post('/', createUserController.handle)
-usersRoutes.patch('/avatar', updateUserAvatarController.handle)
+usersRoutes.use(ensureAthenticated)
+usersRoutes.patch('/avatar', uploadAvatar.single("avatar"), updateUserAvatarController.handle)
 
 export { usersRoutes }
