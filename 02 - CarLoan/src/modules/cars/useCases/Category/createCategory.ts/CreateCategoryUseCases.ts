32a2013df4654eb1fc6411504a9f99060636c017
Tundê <ayotunde_sales@hotmail.com>
2022-03-11 14:33:21 -0300

    fix(tsconfig): fix imports errors

--- 02 - CarLoan/src/modules/cars/useCases/Category/createCategory.ts/CreateCategoryUseCases.ts
+++ 02 - CarLoan/src/modules/cars/useCases/Category/createCategory.ts/CreateCategoryUseCases.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from "tsyringe"
-import { AppError } from "../../../../../errors/AppError"
-import { ICategoriesRepository } from "../../../repositories/Category/ICategoriesRepository"
+import { AppError } from "@errors/AppError"
+import { ICategoriesRepository } from "@modules/cars/repositories/Category/ICategoriesRepository"
 
 interface ICreateCategoryRequest {
   name: string
